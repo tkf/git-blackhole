1305768fcad92aba647d620e2935b950331752e7
Takafumi Arakaki <aka.tkf@gmail.com>
2016-04-21 22:27:21 +0200

    Add setup.py etc. generated from a template

---
+++ setup.py
@@ -0,0 +1,23 @@
+from distutils.core import setup
+
+import git_blackhole
+
+setup(
+    name='git_blackhole',
+    version=git_blackhole.__version__,
+    py_modules=['git_blackhole'],
+    author=git_blackhole.__author__,
+    author_email='aka.tkf@gmail.com',
+    # url='https://github.com/tkf/git_blackhole',
+    license=git_blackhole.__license__,
+    # description='git_blackhole - THIS DOES WHAT',
+    long_description=git_blackhole.__doc__,
+    # keywords='KEYWORD, KEYWORD, KEYWORD',
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        # see: http://pypi.python.org/pypi?%3Aaction=list_classifiers
+    ],
+    install_requires=[
+        # 'SOME_PACKAGE',
+    ],
+)
