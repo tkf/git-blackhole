8b8f71128fe2a41cdb9b961019e35fe4652c4f12
Takafumi Arakaki <aka.tkf@gmail.com>
2016-04-22 20:14:28 +0200

    Refactor test_git_tools.py

--- tests/test_git_tools.py
+++ tests/test_git_tools.py
@@ -1,7 +1,7 @@
 import unittest
 from subprocess import check_call, check_output
 
-from .testutils import MixInGitRepos
+from .testutils import MixInGitRepoPerMethod
 from git_blackhole import getconfig, git_annot_commit
 
 
@@ -14,31 +14,28 @@ def commitchange(file='README', change='change',
                 '--', file])
 
 
-class TestGitTools(MixInGitRepos, unittest.TestCase):
-
-    repos = ['repo']
+class TestGitConfig(MixInGitRepoPerMethod, unittest.TestCase):
 
     def test_remote_url(self):
-        with self.at('repo'):
-            check_call(['git', 'remote', 'add', 'origin', '/dev/null'])
-            assert getconfig('remote.origin.url') == '/dev/null'
+        check_call(['git', 'remote', 'add', 'origin', '/dev/null'])
+        assert getconfig('remote.origin.url') == '/dev/null'
 
     def test_multivar(self):
-        with self.at('repo'):
-            check_call(['git', 'config', '--add', 'spam.egg', 'super'])
-            check_call(['git', 'config', '--add', 'spam.egg', 'delicious'])
-            assert getconfig('spam.egg', aslist=True) == ['super', 'delicious']
+        check_call(['git', 'config', '--add', 'spam.egg', 'super'])
+        check_call(['git', 'config', '--add', 'spam.egg', 'delicious'])
+        assert getconfig('spam.egg', aslist=True) == ['super', 'delicious']
 
     def test_non_existing_config(self):
-        with self.at('repo'):
-            assert getconfig('non.existing.config') is None
+        assert getconfig('non.existing.config') is None
+
+
+class TestGitTools(MixInGitRepoPerMethod, unittest.TestCase):
 
     def test_annot_commit(self):
-        with self.at('repo'):
-            commitchange()
-            check_call(['git', 'checkout', '-b', 'newbranch'])
-            commitchange()
-            check_call(['git', 'checkout', 'master'])
-            rev = git_annot_commit('Annotate newbranch', 'newbranch')
-            out = check_output(['git', 'show', rev])
-            assert b'Annotate newbranch' in out
+        commitchange()
+        check_call(['git', 'checkout', '-b', 'newbranch'])
+        commitchange()
+        check_call(['git', 'checkout', 'master'])
+        rev = git_annot_commit('Annotate newbranch', 'newbranch')
+        out = check_output(['git', 'show', rev])
+        assert b'Annotate newbranch' in out
--- tests/testutils.py
+++ tests/testutils.py
@@ -5,16 +5,7 @@ import tempfile
 from contextlib import contextmanager
 
 
-class MixInTemporaryDirectory(object):
-
-    def setUp(self):
-        self.tmpdir = tempfile.mkdtemp('.git-blackhole-test')
-
-    def tearDown(self):
-        shutil.rmtree(self.tmpdir)
-
-    def tmppath(self, *subpaths):
-        return os.path.join(self.tmpdir, *subpaths)
+class BaseMixInTempDir(object):
 
     @contextmanager
     def at(self, *subpaths):
@@ -25,20 +16,97 @@ class MixInTemporaryDirectory(object):
         os.chdir(orig)
 
 
-class MixInGitRepos(MixInTemporaryDirectory):
+def _setUp_TempDir(self):
+    self.tmpdir = tempfile.mkdtemp(prefix='git-blackhole-test-')
+
+
+def _tearDown_TempDir(self):
+    shutil.rmtree(self.tmpdir)
+
+
+def _tmppath(self, *subpaths):
+    return os.path.join(self.tmpdir, *subpaths)
+
+
+class MixInTempDirPerMethod(BaseMixInTempDir):
+    setUp = _setUp_TempDir
+    tearDown = _tearDown_TempDir
+    tmppath = _tmppath
+
+
+class MixInTempDirPerClass(BaseMixInTempDir):
+    setUpClass = classmethod(_setUp_TempDir)
+    tearDownClass = classmethod(_tearDown_TempDir)
+    tmppath = classmethod(_tmppath)
+
+
+def _prepare_repo(self, repo):
+    subprocess.check_call(['git', 'init'] + (
+        ['--bare'] if repo.endswith('.git') else []
+    ) + [repo], cwd=self.tmpdir)
+
+    for cmd in [
+            ['git', 'config', 'user.email', 'test@blackhole'],
+            ['git', 'config', 'user.name', 'Test Black-Hole'],
+    ]:
+        subprocess.check_call(cmd, cwd=self.tmppath(repo))
+
+
+def _setUp_GitRepo(self):
+    _prepare_repo(self, self.repo)
+    self.orig = os.getcwd()
+    cwd = self.tmppath(self.repo)
+    os.chdir(cwd)
+
+
+def _tearDown_GitRepo(self):
+    os.chdir(self.orig)
+
+
+class MixInGitRepoPerMethod(MixInTempDirPerMethod):
+
+    repo = 'repo'
+
+    def setUp(self):
+        super(MixInGitRepoPerMethod, self).setUp()
+        _setUp_GitRepo(self)
+
+    def tearDown(self):
+        _tearDown_GitRepo(self)
+        super(MixInGitRepoPerMethod, self).tearDown()
+
+
+class MixInGitRepoPerClass(MixInTempDirPerClass):
+
+    repo = 'repo'
+
+    def setUpClass(cls):
+        super(MixInGitRepoPerClass, cls).setUp()
+        _setUp_GitRepo(cls)
+
+    def tearDownClass(cls):
+        _tearDown_GitRepo(cls)
+        super(MixInGitRepoPerClass, cls).tearDownClass()
+
+
+def _setUp_GitRepos(self):
+    for repo in self.repos:
+        _prepare_repo(self, repo)
+
+
+class MixInGitReposPerMethod(MixInTempDirPerMethod):
 
     repos = []
 
     def setUp(self):
-        super(MixInGitRepos, self).setUp()
+        super(MixInGitReposPerMethod, self).setUp()
+        _setUp_GitRepos(self)
 
-        for repo in self.repos:
-            subprocess.check_call(['git', 'init'] + (
-                ['--bare'] if repo.endswith('.git') else []
-            ) + [repo], cwd=self.tmpdir)
 
-            for cmd in [
-                    ['git', 'config', 'user.email', 'test@blackhole'],
-                    ['git', 'config', 'user.name', 'Test Black-Hole'],
-            ]:
-                subprocess.check_call(cmd, cwd=self.tmppath(repo))
+class MixInGitReposPerClass(MixInTempDirPerClass):
+
+    repos = []
+
+    def setUpClass(cls):
+        super(MixInGitReposPerClass, cls).setUp()
+        _setUp_GitRepos(cls)
