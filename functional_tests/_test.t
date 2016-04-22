c87596e4cd2d9856e3252832e2ea2d8fc6276bf3
Takafumi Arakaki <aka.tkf@gmail.com>
2016-04-22 09:05:32 +0200

    Install git-blackhole via setup.py

---
+++ functional_tests/_test.t
@@ -0,0 +1,4 @@
+Check that ``git-blackhole`` is installed in ``.tox/cram*/bin/``::
+
+  $ which git-blackhole
+  */.tox/cram*/bin/git-blackhole (glob)
--- setup.py
+++ setup.py
@@ -20,4 +20,7 @@ setup(
     install_requires=[
         # 'SOME_PACKAGE',
     ],
+    entry_points={
+        'console_scripts': ['git-blackhole = git_blackhole:main'],
+    },
 )
