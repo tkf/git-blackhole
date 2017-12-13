55f2d072175892805461c698c08f20f5291866a5
Takafumi Arakaki <aka.tkf@gmail.com>
2017-12-12 23:21:24 -0800

    Bump to 0.1.0

--- git_blackhole.py
+++ git_blackhole.py
@@ -44,7 +44,7 @@ import os
 import sys
 from subprocess import check_output, CalledProcessError
 
-__version__ = '0.0.0'
+__version__ = '0.1.0'
 __author__ = 'Takafumi Arakaki'
 __license__ = 'BSD-2-Clause'  # SPDX short identifier
 
